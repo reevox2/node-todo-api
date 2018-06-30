0bfa7724a873f02ca6fe8f9913944a003c1e2762
Joel Morancy <morancy.joel@gmail.com>
2018-06-30 11:01:25 +0900

    added GET route for an array of todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,12 @@ app.post('/todos', (req, res)=>{
 	})
 });
 
+app.get('/todos', (req, res)=>{
+	Todo.find().then(todos => {
+		res.status(200).send({todos});
+	})
+	.catch(e => done(e));
+})
 
 app.listen(3000, ()=>{
 	console.log(`Started on port 3000`)
